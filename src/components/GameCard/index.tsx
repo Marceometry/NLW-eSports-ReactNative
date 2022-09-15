5264c267c4d09ac4635e9b450194787db56fd087
Marceometry <marcelino.teixeira.dev@gmail.com>
2022-09-15 12:51:25 -0300

    feat: finish home page

---
+++ src/components/GameCard/index.tsx
@@ -0,0 +1,34 @@
+import { LinearGradient } from 'expo-linear-gradient'
+import {
+  ImageBackground,
+  ImageSourcePropType,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+} from 'react-native'
+import { THEME } from '../../theme'
+import { styles } from './styles'
+
+export interface GameCardProps {
+  id: string
+  name: string
+  ads: string
+  cover: ImageSourcePropType
+}
+
+interface Props extends TouchableOpacityProps {
+  data: GameCardProps
+}
+
+export const GameCard = ({ data }: Props) => {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <ImageBackground source={data.cover} style={styles.cover}>
+        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+          <Text style={styles.name}>{data.name}</Text>
+          <Text style={styles.ads}>{data.ads} anúncios</Text>
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
+  )
+}
